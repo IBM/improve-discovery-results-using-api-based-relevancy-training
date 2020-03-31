b099b2432a2d6d2f0ec5d71b031f4fec0f502e80
Muralidhar Chavan <muralidhar.chavan@in.ibm.com>
2020-03-31 11:03:04 +0530

    Remove authentication details

--- DiscoveryDetails.py
+++ DiscoveryDetails.py
@@ -4,9 +4,9 @@ from requests.auth import HTTPBasicAuth
 from ibm_cloud_sdk_core.api_exception import ApiException
 
 
-apikey = 'E29xF0m167noCwt-6HvyUjGBrG7GGXc-GyEwehk35WU_'
-collection_id = 'e0e5efd0-f21e-4b10-9970-b0a7fcb6f236'
-environment_id = 'e5334501-92ee-40ea-9850-3467f21829a8'
+apikey = 'xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxx'
+collection_id = 'xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx'
+environment_id = 'xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx'
 
 
 authenticator = IAMAuthenticator(apikey)
